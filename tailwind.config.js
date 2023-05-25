6dc44525ebcb4d063a303692fbc7ad23e5cf6098
zuhranahmed <zuhranahmed@gmail.com>
2023-05-25 17:24:15 +0500

    First commit for assesment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        custom: "#95CCC9",
+      },
+    },
+  },
+  plugins: [],
+};
